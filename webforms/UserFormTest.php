b470f36bda6232c391ed372f278c065d8c345cdd
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-20 00:07:46 -0500

    refactoring UserFormTest

--- webforms/UserFormTest.php
+++ webforms/UserFormTest.php
@@ -3,19 +3,17 @@
 /**
  * Test para formulario web : Agregar usuario.
  *
- * @author Gonzalo Chacaltana B <gchacaltanab@gmail.com>
+ * @author Gonzalo Chacaltana <gchacaltanab@outlook.com>
  * @name UserFormTest
  * @category WebTestCase
  */
 require_once(dirname(__FILE__) . '/../simpletest/autorun.php');
 require_once(dirname(__FILE__) . '/../simpletest/web_tester.php');
 
-class UserFormTest extends WebTestCase
-{
+class UserFormTest extends WebTestCase {
 
-    function testDefaultValue()
-    {
-        $this->get('http://localhost/test/SimpleTestPHP/webforms/form.html');
+    function testDefaultValue() {
+        $this->get('http://dev-app/test/SimpleTestPHP/webforms/form.html');
         $this->assertField('name', 'Gonzalo');
         $this->setField('name', 'Javier');
         $this->setField('email', 'gch@aol.com');
@@ -25,9 +23,8 @@ class UserFormTest extends WebTestCase
     /**
      * Testeando un envio de valores por POST a formulario.
      */
-    function testAttemptedHack()
-    {
-        $this->post('http://localhost/demo/index.php?r=site/addUser', array('name' => 'admin'));
+    function testAttemptedHack() {
+        $this->post('http://dev-app/demo/index.php?r=site/addUser', array('name' => 'admin'));
         $this->assertText('Usuario creado');
     }
 
