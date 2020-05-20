ebff50b026f9324fa5158a2e8f6b65ee2d0325b8
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-20 00:06:47 -0500

    refactoring Invoice for test

--- Invoice.php
+++ Invoice.php
@@ -3,11 +3,10 @@
 /**
  * Invoice Class
  *
- * @author Gonzalo Chacaltana B. <gchacaltanab@gmail.com>
+ * @author Gonzalo Chacaltana <gchacaltanab@outlook.com>
  * @category class
  */
-class Invoice
-{
+class Invoice {
 
     private $_subtotal;
     private $_tax;
@@ -16,93 +15,70 @@ class Invoice
     private $_discountRate;
     private $_discountAmount;
 
-    public function getSubtotal()
-    {
+    public function getSubtotal() {
         return $this->_subtotal;
     }
 
-    public function setSubtotal($subtotal)
-    {
+    public function setSubtotal($subtotal) {
         $this->_subtotal = $subtotal;
     }
 
-    public function getTax()
-    {
+    public function getTax() {
         return $this->_tax;
     }
 
-    public function getTaxRate()
-    {
+    public function getTaxRate() {
         return $this->_taxRate;
     }
 
-    public function setTaxRate($taxRate)
-    {
+    public function setTaxRate($taxRate) {
         $this->_taxRate = $taxRate;
     }
 
-    public function setTax($tax)
-    {
+    public function setTax($tax) {
         $this->_tax = $tax;
     }
 
-    public function getTotal()
-    {
+    public function getTotal() {
         return $this->_total;
     }
 
-    public function setTotal($total)
-    {
+    public function setTotal($total) {
         $this->_total = $total;
     }
 
-    public function getDiscountRate()
-    {
+    public function getDiscountRate() {
         return $this->_discountRate;
     }
 
-    public function setDiscountRate($discountRate)
-    {
+    public function setDiscountRate($discountRate) {
         $this->_discountRate = $discountRate;
     }
 
-    public function getDiscountAmount()
-    {
+    public function getDiscountAmount() {
         return $this->_discountAmount;
     }
 
-    public function setDiscountAmount($discountAmount)
-    {
+    public function setDiscountAmount($discountAmount) {
         $this->_discountAmount = $discountAmount;
     }
 
-    /**
-     * Class Construct
-     * @access public
-     */
-    public function __construct($subtotal, $taxRate = null, $discountRate = null)
-    {
+    public function __construct($subtotal, $taxRate = NULL, $discountRate = NULL) {
         $this->setSubtotal($subtotal);
         $this->setDiscountRate($discountRate);
-
         if (is_null($taxRate))
             $this->setTaxRate(18);
-
         $this->calculate();
     }
 
     /**
-     * Method
-     * Calculate values of the invoice.
+     * Calculate values invoice.
      * @access private
      */
-    private function calculate()
-    {
+    private function calculate() {
         if (is_numeric($this->getSubtotal())) {
-
             (float) $taxAmount = $this->getSubtotal() * ($this->getTaxRate() / 100);
             $this->setTax($taxAmount);
-
             $this->setDiscountAmount(0);
             if (!is_null($this->getDiscountRate()) && is_numeric($this->getDiscountRate())) {
                 (float) $discountAmount = $this->getSubtotal() * ($this->getDiscountRate() / 100);
@@ -110,8 +86,9 @@ class Invoice
             }
             (float) $total = $this->getSubtotal() + $this->getTax() - $this->getDiscountAmount();
             $this->setTotal($total);
-            return true;
+            return TRUE;
         }
-        return false;
+        return FALSE;
     }
+
 }
--- InvoiceTest.php
+++ InvoiceTest.php
@@ -3,40 +3,34 @@
 /**
  * InvoiceTest Class
  *
- * @author Gonzalo Chacaltana B <gchacaltanab@gmail.com>
+ * @author Gonzalo Chacaltana <gchacaltanab@outlook.com>
  * @name InvoiceTestCase
  * @category UnitTestCase
  */
 require_once(dirname(__FILE__) . 'simpletest/autorun.php');
 require_once(dirname(__FILE__) . '/Invoice.php');
 
-class InvoiceTestCase extends UnitTestCase
-{
+class InvoiceTestCase extends UnitTestCase {
 
     public $obj;
 
-    public function setUp()
-    {
-        $this->obj = new Invoice(2000, null, 5);
+    public function setUp() {
+        $this->obj = new Invoice(2000, NULL, 5);
     }
 
-    function testSubTotal()
-    {
+    function testSubTotal() {
         $this->assertTrue(is_numeric($this->obj->getSubtotal()), "Se esperaba un valor numérico");
     }
 
-    function testTaxDefault()
-    {
+    function testTaxDefault() {
         $this->assertEqual(360, $this->obj->getTax());
     }
 
-    function testTotalAmount()
-    {
+    function testTotalAmount() {
         $this->assertEqual(2260, $this->obj->getTotal());
     }
 
-    function testTaxAmount()
-    {
+    function testTaxAmount() {
         $this->assertEqual(100, $this->obj->getDiscountAmount());
     }
 
